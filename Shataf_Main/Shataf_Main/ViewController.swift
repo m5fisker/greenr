e91073db8baf36d370ca4615df883247f2284a58
m5fisker <sooryaku1@live.co.uk>
2020-02-11 00:16:12 +0400

    Add files via upload

---
+++ Shataf_Main/Shataf_Main/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Shataf_Main
+//
+//  Created by Kumar Soorya on 10/02/2020.
+//  Copyright © 2020 Kumar Soorya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
